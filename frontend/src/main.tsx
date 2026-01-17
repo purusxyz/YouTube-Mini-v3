5e25828c194b68669e3a837362832a262c63be0c
Purus <purusottampaswan925@gmail.com>
2026-01-17 19:33:26 +0530

    v1.7

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles.css'
 import { AppProvider } from './context/AppContext'
+import axios from 'axios'
+
+const isProd = import.meta.env.PROD
+
+axios.defaults.baseURL = isProd
+  ? import.meta.env.VITE_API_URL
+  : 'http://localhost:5000'
+
+axios.defaults.withCredentials = true
+
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
